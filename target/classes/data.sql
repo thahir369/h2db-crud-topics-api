6fa50a28a684c68080326caa272cff2da023d9d9
ahmedthahir <ahmedthahir369@gmail.com>
2021-01-21 19:03:17 +0530

    added gitignore file

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Topic (id,name,description) VALUES ('1','java','programing language');
+
+INSERT INTO Topic (id,name,description) VALUES ('2', 'spring', 'framework');
+
+INSERT INTO Topic (id,name,description) VALUES ('3', 'mysql', 'database');
+
+INSERT INTO Topic (id,name,description) VALUES ('4', 'angular', 'frontend');
