9404f691b8feac11119d6f250988aa1c86672ac1
ahmedthahir <ahmedthahir369@gmail.com>
2021-02-22 15:53:16 +0530

    Added custom exception

---
+++ src/main/java/com/example/topicapijpa/exception/ExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.topicapijpa.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    //handle specific exception
+    @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+
+        ErrorDetails errorDetails=new ErrorDetails(
+                new Date(),exception.getMessage(),webRequest.getDescription(false),HttpStatus.NOT_FOUND
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
